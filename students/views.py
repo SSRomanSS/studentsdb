f2a3f42fe86e132eafb4430bc2815d99ba19bb23
Roman Shkapiak <r.shkapiak@gmail.com>
2019-07-16 14:58:43 +0300

    Add favicon.ico, refactor students_list.html with cycle, clean up code

--- students/views.py
+++ students/views.py
@@ -1,31 +1,65 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Views for Students
 def students_list(request):
-	return render(request, 'students/students_list.html', {})
-	
+    students = (
+        {
+            'id': 1,
+            'first_name': 'Віталій',
+            'last_name': 'Подоба',
+            'ticket': 235,
+            'image': 'img/me.jpeg'
+        },
+        {
+            'id': 2,
+            'first_name': 'Корост',
+            'last_name': 'Андрій',
+            'ticket': 2123,
+            'image': 'img/piv.png'
+        },
+        {
+            'id': 3,
+            'first_name': 'Притула',
+            'last_name': 'Тарас',
+            'ticket': 5332,
+            'image': 'img/podoba3.jpg'
+        }
+    )
+    return render(request, 'students/students_list.html', {'students': students})
+
+
 def students_add(request):
-	return HttpResponse('<h1>Student Add Form</h1>')
-	
+    return HttpResponse('<h1>Student Add Form</h1>')
+
+
 def students_edit(request, sid):
-	return HttpResponse('<h1>Edit Student {}</h1>'.format(sid))
-	
+    return HttpResponse('<h1>Edit Student {}</h1>'.format(sid))
+
+
 def students_delete(request, sid):
-	return HttpResponse('<h1>Delete Student {}</h1>'.format(sid))
-	
+    return HttpResponse('<h1>Delete Student {}</h1>'.format(sid))
+
+
 # Views for Groups
 def groups_list(request):
-	return render(request, 'students/groups_list.html', {})
-	
+    return render(request, 'students/groups_list.html', {})
+
+
 def groups_add(request):
-	return HttpResponse('<h1>Group Add Form</h1>')
+    return HttpResponse('<h1>Group Add Form</h1>')
+
 
 def groups_edit(request, gid):
-	return HttpResponse('<h1>Edit Group {}</h1>'.format(gid))
+    return HttpResponse('<h1>Edit Group {}</h1>'.format(gid))
+
 
 def groups_delete(request, gid):
-	return HttpResponse('<h1>Delete Group {}/h1>'.format(gid))
-	
+    return HttpResponse('<h1>Delete Group {}/h1>'.format(gid))
+
+
 def students_visiting(request):
-	return HttpResponse('<h1>Student Visiting</h1>')
+    return HttpResponse('<h1>Student Visiting</h1>')
