23473742cc8aa4b50b8559dfaac36103bf6d7f74
Roman Shkapiak <r.shkapiak@gmail.com>
2019-07-19 18:59:41 +0300

    Add base.html, refactor groups_list.html and students_list.html, clean up code

--- students/views.py
+++ students/views.py
@@ -46,7 +46,24 @@ def students_delete(request, sid):
 
 # Views for Groups
 def groups_list(request):
-    return render(request, 'students/groups_list.html', {})
+    groups = (
+        {
+            'id': 1,
+            'name': 'МтМ-21',
+            'leader': 'Ячменев Олег',
+        },
+        {
+            'id': 2,
+            'name': 'МтМ-22',
+            'leader': 'Віталій Подоба',
+        },
+        {
+            'id': 3,
+            'name': 'МтМ-23',
+            'leader': 'Іванов Андрій',
+        }
+    )
+    return render(request, 'students/groups_list.html', {'groups': groups})
 
 
 def groups_add(request):
