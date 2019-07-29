080b6e2acec82afff1c68df86eee69214e878523
Roman Shkapiak <r.shkapiak@gmail.com>
2019-07-29 16:12:44 +0300

    Added requirements.txt, added possibility of sorting in table of students by different columns

--- students/views/students.py
+++ students/views/students.py
@@ -7,17 +7,24 @@ from ..models import Student
 
 # Views for Students
 def students_list(request):
-    students = Student.objects.all()
-    return render(request, 'students/students_list.html', {'students': students})
+	students = Student.objects.all()
+	
+	# try to order students list
+	order_by = request.GET.get('order_by', '')
+	if order_by in ('last_name', 'first_name', 'ticket'):
+		students = students.order_by(order_by)
+	if request.GET.get('reverse', '') == '1':
+		students = students.reverse()
+	return render(request, 'students/students_list.html', {'students': students})
 
 
 def students_add(request):
-    return HttpResponse('<h1>Student Add Form</h1>')
+	return HttpResponse('<h1>Student Add Form</h1>')
 
 
 def students_edit(request, sid):
-    return HttpResponse('<h1>Edit Student {}</h1>'.format(sid))
+	return HttpResponse('<h1>Edit Student {}</h1>'.format(sid))
 
 
 def students_delete(request, sid):
-    return HttpResponse('<h1>Delete Student {}</h1>'.format(sid))
+	return HttpResponse('<h1>Delete Student {}</h1>'.format(sid))
