5857c88ec71c40a7d0a48dc70f31ca18a6bab019
Roman Shkapiak <r.shkapiak@gmail.com>
2019-07-31 16:35:18 +0300

    Added css for table, clean up code

--- students/views/students.py
+++ students/views/students.py
@@ -9,10 +9,10 @@ from ..models import Student
 
 # Views for Students
 def students_list(request):
-	students = Student.objects.all()
+	students = Student.objects.all().order_by('last_name')
 	
 	# try to order students list
-	order_by = request.GET.get('order_by', 'last_name')
+	order_by = request.GET.get('order_by', '')
 	if order_by in ('last_name', 'first_name', 'ticket'):
 		students = students.order_by(order_by)
 	if request.GET.get('reverse', '') == '1':
@@ -27,8 +27,7 @@ def students_list(request):
 		# If page is not an integer, deliver first page.
 		students = paginator.page(1)
 	except EmptyPage:
-		# If page is out of range (e.g. 9999), deliver
-		# last page of results.
+		# If page is out of range (e.g. 9999), deliver last page of results.
 		students = paginator.page(paginator.num_pages)
 	
 	return render(request, 'students/students_list.html', {'students': students})
