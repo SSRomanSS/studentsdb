4ac6462ad86068b291dd68193e7a5205f3c7e219
Roman Shkapiak <r.shkapiak@gmail.com>
2019-08-01 12:32:32 +0300

    Added model for Group, created demo_data file, clean up code

--- students/models.py
+++ students/models.py
@@ -15,7 +15,7 @@ class Student(models.Model):
 		max_length=256,
 		blank=False,
 		verbose_name='Ім’я')
-
+	
 	last_name = models.CharField(
 		max_length=256,
 		blank=False,
@@ -36,7 +36,7 @@ class Student(models.Model):
 		blank=True,
 		verbose_name='Фото',
 		null=True)
-
+	
 	ticket = models.CharField(
 		max_length=256,
 		blank=False,
@@ -46,5 +46,42 @@ class Student(models.Model):
 		blank=True,
 		verbose_name='Додаткові нотатки')
 	
+	student_group = models.ForeignKey(
+		'Group',
+		verbose_name='Група',
+		blank=False,
+		null=True,
+	    on_delete=models.PROTECT)
+	
 	def __str__(self):
 		return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Group(models.Model):
+	"""Group Model"""
+	
+	class Meta(object):
+		verbose_name = 'Група'
+		verbose_name_plural = 'Групи'
+	
+	title = models.CharField(
+		max_length=256,
+		blank=False,
+		verbose_name='Назва')
+	
+	leader = models.OneToOneField(
+		'Student',
+		verbose_name='Староста',
+		blank=True,
+		null=True,
+		on_delete=models.SET_NULL)
+	
+	notes = models.TextField(
+		blank=True,
+		verbose_name='Додаткові нотатки')
+	
+	def __str__(self):
+		if self.leader:
+			return '{} ({} {})'.format(self.title, self.leader.first_name, self.leader.last_name)
+		return '{}'.format(self.title)
+
