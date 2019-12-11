cb3ee715bfa7aa4ceae307d13e843e4ad23174a0
Roman Shkapiak <r.shkapiak@gmail.com>
2019-12-11 14:36:31 +0200

    Added groups add form

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
 	re_path(r'^students/(?P<pk>\d+)/delete/$', students.StudentDeleteView.as_view(), name='students_delete'),
 
 	re_path(r'^groups/$', groups.groups_list, name='groups'),
-	re_path(r'^groups/add/$', groups.groups_add, name='groups_add'),
+	re_path(r'^groups/add/$', groups.GroupsAddView.as_view(), name='groups_add'),
 	re_path(r'^groups/(?P<gid>\d+)/edit/$', groups.groups_edit, name='groups_edit'),
 	re_path(r'^groups/(?P<slug>\d+)/students/$', groups.StudentsOfGroup.as_view(), name='groups_students'),
 	re_path(r'^groups/(?P<gid>\d+)/delete/$', groups.groups_delete, name='groups_delete'),
